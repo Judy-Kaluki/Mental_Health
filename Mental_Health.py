5257f8a0f5ccf9f9b8b6b439fef93dd6bca980b6
Judy-Kaluki <158562551+Judy-Kaluki@users.noreply.github.com>
2024-06-04 22:06:01 +0300

    Update Mental_Health.py

--- Mental_Health.py
+++ Mental_Health.py
@@ -3,162 +3,75 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import io
 
+st.cache
 # Read the CSV files
 
-df_daly=pd.read_csv("Primary_Fact_GlobalDALY_All.csv")
-df_governance=pd.read_csv('Primary_Fact_GovernanceMentalHealth.csv')
-df_spending=pd.read_csv('Primary_Fact_GovernmentSpendingMentalHealth.csv')
-df_GDP=pd.read_csv("Primary_Fact_IMHE_GDP+HealthSpend.csv")
-df_Age=pd.read_csv('Primary_Fact_MentalDisorder_Gender_Age_2000-17.csv')
-df_population=pd.read_csv('Primary_Fact_Population_2000-17.csv')
-df_support=pd.read_csv('Primary_Fact_WHOSupportServicesAvail.csv')
-df_countries=pd.read_csv('Primary_Dim_Countries.csv')
+df_daly = pd.read_csv("Combined data_DALY.csv")
+df_countries = pd.read_csv("Primary_Dim_Countries.csv")
 
-st.cache_data
+# Convert to streamlit dataframes
+st.dataframe(df_daly)
+st.dataframe(df_countries)
 
+#Display column names and data types
+buffer = io.StringIO()
+df_daly.info(buf=buffer)
+s = buffer.getvalue()
 
-# Create a list of all DataFrame
+st.text(s)
 
-dfs_All=[df_daly,df_governance,df_spending,df_GDP,df_Age,df_population,df_support,df_countries]
+buffer = io.StringIO()
+df_countries.info(buf=buffer)
+s = buffer.getvalue()
 
-#List of DataFrame names
-dfs_Names=['df_daly','df_governance','df_spending','df_GDP','df_Age','df_population','df_support','df_countries']
+st.text(s)
+st.markdown(''' Get unique values and their count in the mental health legislation, policy and professional columns''')
 
-#Create Streamlit Data Frames
-
-for df, df_name in zip(dfs_All, dfs_Names):
-      st.subheader(f"Dataframe: {df_name}")
-      st.dataframe(df)
-
-#Given that the df_GDP is too large to display, we shall select specific columns for analysis before proceeding to merge the
-#data to one df
-
-st.subheader('Data Cleaning and Preprocessing')
-
-st.markdown('**DALY DF**')
-st.markdown('''From the DALY dataframe, the rows under the metric percent will be selected, as number and rate are different measures
-            for the same observation. Then, rename the columns removing the spaces, and convert the year column to a date data type
-''')
-
-df_daly=df_daly[['Cause','Code (Data6)','metric_name', 'sex_name', 'year (Data6)', 'val (Data6)']]
-# filter to only include where the metric measure is percent
-df_daly=df_daly.loc[df_daly['metric_name']=="Percent"]
-#rename the column names
-df_daly_r=df_daly.rename(columns={'Code (Data6)': 'code', 'year (Data6)': 'year', 'val (Data6)': 'daly%','sex_name':'sex'})
-#convert the year column to numeric values as non-numeric strings are contained in the data
-#df_daly_r=pd.to_numeric(df_daly_r['year'].astype(str).str.replace(r'1/1/', '', regex=True))
+st.write(df_daly.Mental_health_legislation.value_counts())
+st.write(df_daly.Mental_health_policy.value_counts())
+st.write(df_daly.Mental_health_professional.value_counts())
 
 
-st.markdown('**GOVERNANCE DF**')
-# Select necessary columns
-df_governance=df_governance[['Code (Data)','Stand-alone mental health legislation','Stand-alone policy or plan for mental health', 'Year (Data)']]
-#rename the column names
-df_governance=df_governance.rename(columns={'Code (Data)': 'code', 'Year (Data)': 'year','Stand-alone mental health legislation':'legislation','Stand-alone policy or plan for mental health':'policy'})
-#convert the year column to numeric values
-#df_governance=pd.to_numeric(df_governance['year'].astype(str).str.replace(r'1/1/', '', regex=True))
+st.markdown('''The No data values are too high, so we'll drop these column''')
+df_daly=df_daly.drop(columns=['Mental_health_legislation','Mental_health_policy','Mental_health_professional'], axis=1)
 
-st.markdown('**GDP DF**')
-df_GDP=df_GDP[["Code (Data5)", 'Metric','Year (Data5)','Health Expenditure per Capita (USD)']]
-# rename the column names. Assumption that the expenditure refers to GDP per capita as well
-df_GDP=df_GDP.rename(columns={"Code (Data5)": 'code', 'Year (Data5)': 'year','Health Expenditure per Capita (USD)':'expenditure'})
-#convert the year column to numeric values
-#df_GDP=pd.to_numeric(df_GDP['year'].astype(str).str.replace(r'1/1/', '', regex=True))
+st.markdown('''Merge df_daly with df_countries''')
 
-st.markdown('**Population DF**')
-# Assumption that the expenditure refers to expenditure per capita, because there are instances where the metric name is GDP per capita
-df_population=df_population[['Age Group', "Code", "Sex","Year","Population (#) - Female", "Population (#) - Male"]]
-# Fill missing values in `Population (#) - Female` and `Population (#) - Male` with 0
-df_population[['Population (#) - Female', 'Population (#) - Male']] = df_population[['Population (#) - Female', 'Population (#) - Male']].fillna(0)
-# Add an additional column called Population, sex is identified under the sex column
-df_population['Population'] = df_population['Population (#) - Female'] + df_population['Population (#) - Male']
-# Select the necessary columns
-df_population= df_population[['Population', 'Age Group', 'Code', 'Sex', 'Year']]
-df_population=df_population.rename(columns={'Year':'year','Code':'code','Sex':'sex'})
-#convert the year column to numeric values
-#df_population=pd.to_numeric(df_population['year'].astype(str).str.replace(r'1/1/', '', regex=True))
+# Rename columns for merging
+df_countries = df_countries.rename(columns={'Alpha-3 code': 'Country_Code'})
+df_daly = df_daly.rename(columns={'Code (Data6)': 'Country_Code'})
 
-st.markdown('**Age DF**')
-#Select the necessary columns and filter the data for only percent values
-df_Age=df_Age[['age','cause','Code (Data 1+)', 'metric','sex','year','val']]
-#Select rows where metric=percent
-df_Age=df_Age.loc[df_Age['metric']=="Percent"]
-#Rename columns
-df_Age=df_Age.rename(columns={'Code (Data 1+)':'code'})
-#convert the year column to numeric values
-#df_Age=pd.to_numeric(df_Age['year'].astype(str).str.replace(r'1/1/', '', regex=True))
-
-st.markdown('**Spending, Support**')
-#renaming the columns
-df_spending=df_spending.rename({'Code (Data3)': 'code', 'Government Expenditure on Mental Health': 'expenditure_mental health'})
-
-df_support=df_support.rename(columns={'Code (Data4)': 'code', 'Year (Data4)': 'year'})
-#df_support=pd.to_numeric(df_support['year'].astype(str).str.replace(r'1/1/', '', regex=True))
-
-#convert the year columns to date datatype
-for df in[df_daly_r,df_governance,df_GDP,df_Age,df_support,df_population]:
-     df['year']=pd.to_datetime(df['year'], format="%d/%m/%Y",errors='coerce').dt.year
-
-#convert the year columns in population to date datatype
-
-df_daly_r,df_governance,df_GDP,df_population,df_Age,df_spending,df_support,df_countries
-#Create two data Frames (Focus on the disability adjusted life years)
-merged_df=pd.merge(df_daly_r,df_countries, left_on='code', right_on='Alpha-3 code', how='left')
-merged_df=pd.merge(merged_df,df_Age,on=['code','year','sex'], how='inner')
-merged_df=pd.merge(merged_df,df_GDP,on=['code','year'], how='left')
-merged_df=pd.merge(merged_df,df_governance,on=['code','year'], how='left')
-merged_df=pd.merge(merged_df,df_spending,on=['code'], how='left')
-merged_df=pd.merge(merged_df,df_support,on=['code'], how='left')
-
-merged_df=pd.merge(merged_df,df_population,on=['code','year','sex'], how='inner')
-
-
+# Merge the dataframes
+merged_df=df_daly.merge(df_countries, on='Country_Code', how='left')
 st.dataframe(merged_df)
 
-#Create an updated list with the updated dfs then check for null values
-dfs_Updated=[df_daly,df_governance,df_spending,df_GDP,df_Age,df_population,df_support,df_countries]
-dfsNames_Updated=['df_daly','df_governance','df_spending','df_GDP','df_Age','df_population','df_support','df_countries']
-
-#Create Streamlit Data Frames
-for df, df_name in zip(dfs_Updated, dfsNames_Updated):
-      st.subheader(f"Dataframe: {df_name}")
-      st.dataframe(df)
+st.markdown('We shall drop redundant columns from the data frame')
+merged_df=merged_df.drop(columns=['Country_Code','Country Code','Country_Orig','Income group (group)'], axis=1)
 
 
+#Display column names and data types
+buffer = io.StringIO()
+merged_df.info(buf=buffer)
+s = buffer.getvalue()
+st.text(s)
 
 
+st.subheader('Descriptive Statistics')
+# Summary of the numercial columns
+st.write(merged_df.describe())
+
+# Summary of the categorical columns
+st.write(merged_df.describe(include='object'))
+
+st.subheader('Exploratory Data Analysis')
+#check for null values
+st.write(merged_df.isnull().sum())
 
 
-#Loop through the df and display the first five rows
+#check the data for duplicates
+st.write(merged_df.duplicated().sum())
 
-for df,df_name in (dfs_All, dfs_Names):
-    print(f"\nFirst 5 rows of {df_name}:")
-    print(df.head())
+st.markdown('**Outlier Detection**')
 
-# df_daly: This file contains data on the global burden of mental health disease. A DALY is a measure of overall disease
-#burden , expressed as the number of years lost due to disease, in this case mental disorders. The column represents the estimated 
-#DALYs for each cause, entity and year: The dataser appears to cover various mental health conditions including: Anxiety disorders, ADHD,
-# Bipolar, Depression, Eating disroders, schizophrenia 
-# df_governance: the datasets indicates whethere there is a standalone policy for mental health
-#df_spending: This indicates the government health spending on mental health
-#df_GDP: The data set contains information on GDP per country: (explain GDP) and total helth spending
-
-#Print the column names and data types for each
-
-for df,df_name in (dfs_All, dfs_Names):
-    print(f"\nColumn and data types of {df_name}:")
-    print(df.info())
-
-#Selecting the requisite columns
-
-
-#Check for null values
-for df,df_name in (dfs_All, dfs_Names):
-    print(f"\n{df_name}:")
-    print(df.isnull().sum())
-
-#Check for duplicated values
-#Check for null values
-for df,df_name in (dfs_All, dfs_Names):
-    print(f"\n{df_name}:")
-    print(df.duplicated().sum())
